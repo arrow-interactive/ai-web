17983a0c885d411bb5012feec27d66d8e113d766
Mattéo Leullier <matteo@8x13b.tk>
2021-07-12 11:40:45 +0200

    Removed linters from vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
+import Checker from 'vite-plugin-checker';
+import { imagetools } from 'vite-imagetools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact()]
+  plugins: [
+    preact(),
+    Checker({
+      overlay: true,
+      typescript: true,
+    }),
+    imagetools()
+  ]
 })
