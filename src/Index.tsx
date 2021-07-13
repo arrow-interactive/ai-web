7b57da16511a595c42a02113689de7b809bb43bc
Mattéo Leullier <matteo@8x13b.tk>
2021-07-13 11:10:26 +0200

    removed very bad debugging

--- src/Index.tsx
+++ src/Index.tsx
@@ -1,16 +1,4 @@
-import Nav from './components/Nav';
-import { useEffect } from 'preact/hooks';
-
 function App () {
-  useEffect(() => {
-    console.log(Nav);
-  }, [Nav]);
-
-  console.log(Nav);
-
-  // @ts-ignore
-  window.nav = Nav;
-
   return (
     <>
       <p>app works!</p>
