4d6f335320aa47f271a194154509b73921966149
Mattéo Leullier <matteo@8x13b.tk>
2021-07-14 23:04:41 +0200

    fixed jsx errors

--- src/preact.d.ts
+++ src/preact.d.ts
@@ -1,9 +1,3 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-undef */
 import JSX = preact.JSX;
-
-import compat from 'preact/compat';
-
-declare module 'react' {
-  export default compat;
-}
---
+++ src/react.d.ts
@@ -0,0 +1,18 @@
+export {
+  Attributes,
+  FunctionalComponent as SFC,
+  AnyComponent as ComponentType,
+  AnyComponent as JSXElementConstructor,
+  Component as ComponentClass,
+  ClassAttributes,
+  PreactContext as Context,
+  PreactProvider as Provider,
+  VNode as ReactElement,
+  VNode as ReactNode,
+  createElement,
+  Fragment,
+  Ref,
+  render,
+  JSX,
+  RenderableProps as ComponentPropsWithRef
+} from 'preact';
